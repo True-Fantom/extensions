f77b89e6944152e6b3beb16790e10c38b0025568
TrueFantom <116278271+True-Fantom@users.noreply.github.com>
2023-09-20 22:34:22 +0300

    Create variables.js

---
+++ extensions/tests/variables.js
@@ -0,0 +1,132 @@
+(function (Scratch) {
+  "use strict";
+
+  if (!Scratch.extensions.unsandboxed) {
+    throw new Error("Variables Extension must be run Unsandboxed!");
+  }
+
+  const vm = Scratch.vm;
+  const cast = Scratch.Cast;
+
+  const variables = {};
+
+  const getVariableBlocks = () => {
+    const createVariableBlock = (text) => {
+      return {
+        opcode: "getVariable_" + text,
+        blockType: Scratch.BlockType.REPORTER,
+        text: text,
+      };
+    };
+    return Object.keys(variables).map((variable) =>
+      createVariableBlock(variable),
+    );
+  };
+
+  class Extension {
+    getInfo() {
+      return {
+        id: "truefantomvariables",
+        name: "Variables",
+
+        blocks: [
+          {
+            hideFromPalette: Object.keys(variables).length <= 0,
+            opcode: "setVariable",
+            blockType: Scratch.BlockType.COMMAND,
+            text: "set [NAME] to [VALUE]",
+            arguments: {
+              NAME: {
+                type: Scratch.ArgumentType.STRING,
+                menu: "VARIABLES",
+              },
+              VALUE: {
+                type: Scratch.ArgumentType.STRING,
+                defaultValue: "0",
+              },
+            },
+          },
+          {
+            hideFromPalette: Object.keys(variables).length <= 0,
+            opcode: "changeVariable",
+            blockType: Scratch.BlockType.COMMAND,
+            text: "change [NAME] by [VALUE]",
+            arguments: {
+              NAME: {
+                type: Scratch.ArgumentType.STRING,
+                menu: "VARIABLES",
+              },
+              VALUE: {
+                type: Scratch.ArgumentType.NUMBER,
+                defaultValue: 1,
+              },
+            },
+          },
+          {
+            func: "createVariable",
+            blockType: Scratch.BlockType.BUTTON,
+            text: "Make a Variable",
+          },
+          {
+            hideFromPalette: Object.keys(variables).length <= 0,
+            func: "deleteVariable",
+            blockType: Scratch.BlockType.BUTTON,
+            text: "Delete a Variable",
+          },
+          {
+            hideFromPalette: Object.keys(variables).length <= 0,
+            blockType: Scratch.BlockType.LABEL,
+            text: "For all sprites:",
+          },
+          ...getVariableBlocks(),
+        ],
+        menus: {
+          VARIABLES: {
+            acceptReporters: false,
+            items:
+              Object.keys(variables).length > 0 ? Object.keys(variables) : [""],
+          },
+        },
+      };
+    }
+
+    setVariable(args) {
+      variables[args.NAME] = args.VALUE;
+    }
+    changeVariable(args) {
+      variables[args.NAME] =
+        cast.toNumber(variables[args.NAME]) + cast.toNumber(args.VALUE);
+    }
+
+    createVariable() {
+      const name = prompt("New variable name:", "");
+      if (!Object.keys(variables).includes(name) && name !== "") {
+        variables[name] = 0;
+        Extension.prototype["getVariable_" + name] = (args, util, info) => {
+          return variables[info.text];
+        };
+        vm.extensionManager.refreshBlocks();
+      }
+    }
+    deleteVariable() {
+      const name = prompt("Deleted variable name:", "");
+      if (Object.keys(variables).includes(name) && name !== "") {
+        delete variables[name];
+        delete Extension.prototype["getVariable_" + name];
+        vm.extensionManager.refreshBlocks();
+      }
+    }
+  }
+
+  for (let i = 0; i < Object.keys(variables).length; i++) {
+    Extension.prototype["getVariable_" + Object.keys(variables)[i]] = (
+      args,
+      util,
+      info,
+    ) => {
+      return variables[info.text];
+    };
+  }
+
+  Scratch.extensions.register(new Extension());
+})(Scratch);
