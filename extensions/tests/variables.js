d6c4fc437cc3cefd255b11e1ebfbfe8f608c1ffc
TrueFantom <116278271+True-Fantom@users.noreply.github.com>
2023-09-26 18:44:44 +0300

    Update variables.js

--- extensions/tests/variables.js
+++ extensions/tests/variables.js
@@ -8,28 +8,16 @@
   const vm = Scratch.vm;
   const cast = Scratch.Cast;
 
-  const variables = {global: {"my variable": 0}, local: {}};
+  const variables = { "my variable": 0 };
 
-  const getVariableBlocks = (type) => {
-    if (type = "global") {
-      return Object.keys(variables.global).map((variable) => {
-        return {
-          opcode: "global_getVariable_" + variable,
-          blockType: Scratch.BlockType.REPORTER,
-          text: variable,
-        }; 
-      });
-    }
-    else if (type = "local") {
-      return Object.keys(variables.local).map((sprite) => {
-        return {
-          filter: [sprite],
-          opcode: sprite + "_getVariable_" + sprite.variable,
-          blockType: Scratch.BlockType.REPORTER,
-          text: sprite.variable,
-        }; 
-      });
-    }
+  const getVariableBlocks = () => {
+    return Object.keys(variables).map((variable) => {
+      return {
+        opcode: "getVariable_" + variable,
+        blockType: Scratch.BlockType.REPORTER,
+        text: variable,
+      };
+    });
   };
 
   class Extension {
@@ -88,17 +76,11 @@
             text: "For all sprites:",
           },
           ...getVariableBlocks(),
-          {
-            hideFromPalette: Object.keys(variables).length <= 0,
-            blockType: Scratch.BlockType.LABEL,
-            text: "For this sprite only:",
-          },
         ],
         menus: {
           VARIABLES: {
             acceptReporters: false,
-            items:
-              Object.keys(variables).length > 0 ? Object.keys(variables) : [""],
+            items: Object.keys(variables),
           },
         },
       };
@@ -114,14 +96,13 @@
 
     createVariable() {
       const name = prompt("New variable name:", "");
-      const local = confirm("For this sprite only");
-      if (!Object.keys(variables).includes(name) && name !== "" && name !== null) {
-        if (local) {
-          const target = vm.runtime._editingTarget;
-          console.log(target);
-        }
+      if (
+        !Object.keys(variables).includes(name) &&
+        name !== "" &&
+        name !== null
+      ) {
         variables[name] = 0;
-        Extension.prototype["global_getVariable_" + name] = (args, util, info) => {
+        Extension.prototype["getVariable_" + name] = (args, util, info) => {
           return variables[info.text];
         };
         vm.extensionManager.refreshBlocks();
@@ -129,20 +110,20 @@
     }
     deleteVariable() {
       const name = prompt("Deleted variable name:", "");
-      if (Object.keys(variables).includes(name) && name !== "" && name !== null) {
+      if (
+        Object.keys(variables).includes(name) &&
+        name !== "" &&
+        name !== null
+      ) {
         delete variables[name];
-        delete Extension.prototype["global_getVariable_" + name];
+        delete Extension.prototype["getVariable_" + name];
         vm.extensionManager.refreshBlocks();
       }
     }
   }
 
   for (const variable in variables) {
-    Extension.prototype["global_getVariable_" + variable] = (
-      args,
-      util,
-      info,
-    ) => {
+    Extension.prototype["getVariable_" + variable] = (args, util, info) => {
       return variables[info.text];
     };
   }
